b8903fccee96c3668bc4d7c388d84a6a3de3bf6a
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-03-01 16:54:18 +0530

    Add dashboard layout and page with user onboarding check; update dependencies

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"]})
 
@@ -28,6 +29,8 @@ export default function RootLayout({ children }) {
           >
             <Header/>
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors/>
+
             <footer className="bg-muted/50 py-12 ">
               <div className="container mx-auto text-center px-4 text-gray-200">
                 <p>Made with Tushar</p>

822ca87b4047cb28ccbf6150d7131ddb637d7968
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-03-02 19:18:10 +0530

    Implement AI insights generation and update industry insights functionality; refactor imports and dependencies

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,9 @@
-import { inngest } from "@/lib/inngest/client"
-import { serve } from "inngest/next"
+import { serve } from "inngest/next";
 
-export const {GET, POST, PUT} = serve({
-    client: inngest,
-    functions: [
-        
-    ]
-})
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [generateIndustryInsights],
+});
